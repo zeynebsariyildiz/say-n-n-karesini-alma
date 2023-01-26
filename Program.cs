7114276131282df12ca667a32f146db8452164da
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-26 11:46:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sayının_karesini_alma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Lütfen 1. Sayıyı Girirniz");
+            int sayi = Int32.Parse(Console.ReadLine());
+            int karesi = sayi * sayi;
+            Console.WriteLine("Lütfen 2. Sayıyı Giriniz");
+            int sayi = Int32.Parse(Console.ReadLine());
+            int karesi = sayi * sayi;
+            Console.ReadLine();
+
+
+        }
+    }
+}
